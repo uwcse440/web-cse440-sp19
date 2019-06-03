52e7abc94abf49a1ee886f503506d452d3fcdd0c
shourj@uw.edu <Pl@zmatv1>
2019-06-03 16:55:47 -0700

    HYPE image hue

--- projects/hypebox/js/random_box_color_filter.js
+++ projects/hypebox/js/random_box_color_filter.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-    $(".hypebox_trans").filter(270)
+  $(".hypebox_trans").click(function() {
+    var random_deg = Math.floor(Math.random() * 360);
+    var hue = "hue-rotate(" + random_deg + "deg)";
+    $(".hypebox_trans").css("filter", hue);
   });
+});
