34b68552656aaa7ac2ca5f3a9b0ec3f4adbf6d92
shourj@uw.edu <Pl@zmatv1>
2019-06-03 17:02:29 -0700

    Changes hue every 1 sec automatically

--- projects/hypebox/js/random_box_color_filter.js
+++ projects/hypebox/js/random_box_color_filter.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  $(".hypebox_trans").click(function() {
+  setInterval(function(){
     var random_deg = Math.floor(Math.random() * 360);
     var hue = "hue-rotate(" + random_deg + "deg)";
     $(".hypebox_trans").css("filter", hue);
-  });
+  }, 1000);
 });
