ed4bb540cdc2d1bd3b037b43fbbaa213d52ef798
Karan Narula <narulaskaran@gmail.com>
2019-06-10 09:38:04 -0700

    hypebox project website  (#4)
    * supreme who?
    
    * yucky logo iterations. formatting and content cuts
    
    * team members
    
    * panel prototype
    
    * paginate the sections
    
    * added index.html background
    
    * merge w jonathon
    
    * change box strokes to transparent png
    
    * updated iframe
    
    * invert filter and white page bg
    
    * branding
    
    * embedded prototypes
    
    * jquery
    
    * hype block
    
    * hypeclock final
    
    * other pages colors and size
    
    * index page
    
    * split left and right divs on other pages
    
    * shourya is my prophet
    
    * added padding for shourya
    
    * fixed my name:
    
    * buying selling scroll removal
    
    * fixed my name in more place
    
    * padding
    
    * about page
    
    * inverted background
    
    * background for paginate dpages
    
    * links back to main page
    
    * my favorite ta is abhinav
    
    * I also love jeremy
    
    * jonny name
    
    * padding on about
    
    * LAST PADDING I LOVE YOU TAs (abhinav) #nigini #oliviera
    
    * change project_thumb and uupdate references
    
    * add social icons at bottom and remvoe old site files
    
    * updated social icons
    
    * added fade to main page
    
    * Padding changes
    
    * HYPE image hue
    
    * Changes hue every 1 sec automatically
    
    * merged buying and selling
    
    * fixed about extra script tag
    
    * Changed box hue change time to 10 secs
    
    * update transparent image

---
+++ projects/hypebox/viewer/story.js
@@ -0,0 +1,528 @@
+var story = {
+"docName": "hypebox-screens",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"hasRetina": true,
+"disableHotspots": false,
+"pages": [
+$.extend(new ViewerPage(),{
+"index": 0,
+"image": "main_screen.png",
+"image2x": "main_screen@2x.png",
+"width": 375,
+"height": 667,
+"title": "Main Screen",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 375,
+	"numberOfColumns": 12,
+	"columnWidth": 26.25,
+	"gutterWidth": 5
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 107,
+			"y": 528,
+			"width": 179,
+			"height": 57
+		},
+		"isParentFixed": false,
+		"page": 5,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 107,
+			"y": 451,
+			"width": 179,
+			"height": 57
+		},
+		"isParentFixed": false,
+		"page": 1,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 1,
+"image": "browse_screen.png",
+"image2x": "browse_screen@2x.png",
+"width": 375,
+"height": 800,
+"title": "Browse Screen",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 375,
+	"numberOfColumns": 12,
+	"columnWidth": 26.25,
+	"gutterWidth": 5
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 15,
+			"y": 561,
+			"width": 170,
+			"height": 221
+		},
+		"isParentFixed": false,
+		"page": 2,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 21,
+			"y": 33.173333333333176,
+			"width": 8.106666666666683,
+			"height": 13.653333333333336
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 2,
+"image": "product_detail_page.png",
+"image2x": "product_detail_page@2x.png",
+"width": 375,
+"height": 800,
+"title": "Product Detail Page",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 375,
+	"numberOfColumns": 12,
+	"columnWidth": 26.25,
+	"gutterWidth": 5
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 29,
+			"y": 417,
+			"width": 317,
+			"height": 46
+		},
+		"isParentFixed": false,
+		"page": 3,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 0,
+			"y": 20,
+			"width": 375,
+			"height": 40
+		},
+		"isParentFixed": false,
+		"page": 1,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 3,
+"image": "payment_screen.png",
+"image2x": "payment_screen@2x.png",
+"width": 375,
+"height": 667,
+"title": "Payment Screen",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 375,
+	"numberOfColumns": 12,
+	"columnWidth": 26.25,
+	"gutterWidth": 5
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 175,
+			"y": 619,
+			"width": 25,
+			"height": 25
+		},
+		"isParentFixed": false,
+		"page": 2,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 110,
+			"y": 389,
+			"width": 156,
+			"height": 156
+		},
+		"isParentFixed": false,
+		"page": 4,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 4,
+"image": "payment_complete_screen.png",
+"image2x": "payment_complete_screen@2x.png",
+"width": 375,
+"height": 667,
+"title": "Payment Complete Screen",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 375,
+	"numberOfColumns": 12,
+	"columnWidth": 26.25,
+	"gutterWidth": 5
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 175,
+			"y": 619,
+			"width": 25,
+			"height": 25
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 0
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 5,
+"image": "login_screen.png",
+"image2x": "login_screen@2x.png",
+"width": 375,
+"height": 667,
+"title": "Login Screen",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 375,
+	"numberOfColumns": 12,
+	"columnWidth": 26.25,
+	"gutterWidth": 5
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 175,
+			"y": 619,
+			"width": 25,
+			"height": 25
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 0,
+			"y": 20,
+			"width": 375,
+			"height": 40
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 110,
+			"y": 256,
+			"width": 156,
+			"height": 156
+		},
+		"isParentFixed": false,
+		"page": 6,
+		"index": 2
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 6,
+"image": "authentication_screen.png",
+"image2x": "authentication_screen@2x.png",
+"width": 375,
+"height": 667,
+"title": "Authentication Screen",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 375,
+	"numberOfColumns": 12,
+	"columnWidth": 26.25,
+	"gutterWidth": 5
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 175,
+			"y": 619,
+			"width": 25,
+			"height": 25
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 0,
+			"y": 20,
+			"width": 375,
+			"height": 40
+		},
+		"isParentFixed": false,
+		"page": 5,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 107,
+			"y": 458,
+			"width": 161,
+			"height": 44
+		},
+		"isParentFixed": false,
+		"page": 7,
+		"index": 2
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 7,
+"image": "place_in_box_screen.png",
+"image2x": "place_in_box_screen@2x.png",
+"width": 375,
+"height": 667,
+"title": "Place In Box Screen",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 375,
+	"numberOfColumns": 12,
+	"columnWidth": 26.25,
+	"gutterWidth": 5
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 175,
+			"y": 619,
+			"width": 25,
+			"height": 25
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 0,
+			"y": 20,
+			"width": 375,
+			"height": 40
+		},
+		"isParentFixed": false,
+		"page": 6,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 107,
+			"y": 458,
+			"width": 161,
+			"height": 44
+		},
+		"isParentFixed": false,
+		"page": 8,
+		"index": 2
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 8,
+"image": "scanning_screen.png",
+"image2x": "scanning_screen@2x.png",
+"width": 375,
+"height": 667,
+"title": "Scanning Screen",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 375,
+	"numberOfColumns": 12,
+	"columnWidth": 26.25,
+	"gutterWidth": 5
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 175,
+			"y": 619,
+			"width": 25,
+			"height": 25
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 28,
+			"y": 68,
+			"width": 321,
+			"height": 533
+		},
+		"isParentFixed": false,
+		"page": 9,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 9,
+"image": "scanning_screen(1).png",
+"image2x": "scanning_screen(1)@2x.png",
+"width": 375,
+"height": 667,
+"title": "Scanning Screen(1)",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 375,
+	"numberOfColumns": 12,
+	"columnWidth": 26.25,
+	"gutterWidth": 5
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 175,
+			"y": 619,
+			"width": 25,
+			"height": 25
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 113,
+			"y": 506,
+			"width": 161,
+			"height": 44
+		},
+		"isParentFixed": false,
+		"page": 10,
+		"index": 1
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 10,
+"image": "verified_screen.png",
+"image2x": "verified_screen@2x.png",
+"width": 375,
+"height": 667,
+"title": "Verified Screen",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 375,
+	"numberOfColumns": 12,
+	"columnWidth": 26.25,
+	"gutterWidth": 5
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 175,
+			"y": 619,
+			"width": 25,
+			"height": 25
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 0
+	},
+	{
+		"rect": {
+			"x": 175,
+			"y": 619,
+			"width": 25,
+			"height": 25
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 1
+	},
+	{
+		"rect": {
+			"x": 107,
+			"y": 508,
+			"width": 161,
+			"height": 44
+		},
+		"isParentFixed": false,
+		"page": 11,
+		"index": 2
+	}
+],
+})
+,$.extend(new ViewerPage(),{
+"index": 11,
+"image": "payment_complete_screen(1).png",
+"image2x": "payment_complete_screen(1)@2x.png",
+"width": 375,
+"height": 667,
+"title": "Payment Complete Screen(1)",
+'layout' : {
+	"offset": 0,
+	"totalWidth": 375,
+	"numberOfColumns": 12,
+	"columnWidth": 26.25,
+	"gutterWidth": 5
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [
+	{
+		"rect": {
+			"x": 175,
+			"y": 619,
+			"width": 25,
+			"height": 25
+		},
+		"isParentFixed": false,
+		"page": 0,
+		"index": 0
+	}
+],
+})
+   ]
+,"resolutions": [2],
+"zoomEnabled": true,
+"title": "Hypebox Screens",
+"totalImages": 12,
+"highlightLinks": false
+}
